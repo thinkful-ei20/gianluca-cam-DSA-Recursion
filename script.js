6b45ac1b7482128a0de17f8b2b40ead51090af50
patersog <gianluca.paterson@gmail.com>
2018-06-18 12:29:58 -0400

    nth Triangular Number

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 // console.log('Recursion');
 
@@ -24,11 +24,11 @@ function countSheep(num) {
 
 
 /** Array Double
- * Write a function that takes an array as input which contains an unknown set of numbers,
+ *  Write a function that takes an array as input which contains an unknown set of numbers,
  *	and outputs an array with each input value doubled. Test your solution by trying a handful of different arrays. For example,
  *
- * Input: [1, 2, 3]
- * Output: [2, 4, 6]
+ *  Input: [1, 2, 3]
+ *  Output: [2, 4, 6]
  *
  */
 
@@ -50,12 +50,36 @@ Write a function that reverses a string. Take a string as input, reverse the str
 */
 
 const reverseStr = str => {
-  if (str === '') return '';
-  // Extract part of a string, from the end using .substr(-1)
-  // recusive call excluding the last letter of the string
-  return str.substr(-1) + reverseStr(str.substr(0, str.length -1));
+	if (str === '') return '';
+	// Extract part of a string, from the end using .substr(-1)
+	// recusive call excluding the last letter of the string
+	return str.substr(-1) + reverseStr(str.substr(0, str.length -1));
 };
 
-console.log(reverseStr('banana'));
+// console.log(reverseStr('banana'));
 
+/** nth Triangular Number
+ *  Calculates the nth triangular number. A triangular number counts the objects that can form an equilateral triangle.
+ *  The nth triangular number is the number of dots composing a triangle with n dots on a side, and is equal to the sum
+ *  of the n natural numbers from 1 to n.
+ *
+ *  This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45
+ */
+
+
+ /**
+  * 1   i=1  n=1
+  * 3   i=2  n=3
+  * 6   i=3  n=6
+  * 10  i=4  n=10
+  * 15  i=5  n=15
+  */
+
+const nthTriangularNumber = index => {
+	if(index === 0) return 0;
+
+	return index + nthTriangularNumber(index - 1);
+};
+
+console.log(nthTriangularNumber(9));
 
