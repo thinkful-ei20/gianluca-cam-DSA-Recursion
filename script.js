d02afeaaff3ddf4b293c0f6a0e9823d79ed7a576
patersog <gianluca.paterson@gmail.com>
2018-06-18 16:01:38 -0400

    Finish Binary and Factorial

--- script.js
+++ script.js
@@ -2,15 +2,18 @@
 
 // console.log('Recursion');
 
-/*
-Write a recursive function that counts how many sheep jumps over the fence. Your program should take a number as an input. That number should be the number of sheep you have. The function should display the number along with the message "Another sheep jump over the fence" until no more sheep are left.
-
-Input: 3
-Output:
-3 - Another sheep jump over the fence
-2 - Another sheep jump over the fence
-1 - Another sheep jump over the fence
-*/
+/** Write a recursive function that counts how many sheep jumps over the fence.
+ * 
+ *  Your program should take a number as an input. That number should be the number of sheep you have.
+ * The function should display the number along with the message:
+ * 		"Another sheep jump over the fence" until no more sheep are left.
+ *
+ *  Input: 3
+ *  Output:
+ *  3 - Another sheep jump over the fence
+ *  2 - Another sheep jump over the fence
+ *  1 - Another sheep jump over the fence
+ */
 
 function countSheep(num) {
 	if (num === 0) {
@@ -44,10 +47,9 @@ const arrayDouble = arr => {
 // console.log('Input: ', arr);
 // console.log('Output: ', arrayDouble(arr));
 
-/*
-Reverse String
-Write a function that reverses a string. Take a string as input, reverse the string, and return the new string.
-*/
+/** Reverse String
+ *  Write a function that reverses a string. Take a string as input, reverse the string, and return the new string.
+ */
 
 const reverseStr = str => {
 	if (str === '') return '';
@@ -66,15 +68,6 @@ const reverseStr = str => {
  *  This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45
  */
 
-
- /**
-  * 1   i=1  n=1
-  * 3   i=2  n=3
-  * 6   i=3  n=6
-  * 10  i=4  n=10
-  * 15  i=5  n=15
-  */
-
 const nthTriangularNumber = index => {
 	if(index === 0) return 0;
 
@@ -83,10 +76,10 @@ const nthTriangularNumber = index => {
 
 // console.log(nthTriangularNumber(9));
 
-/*
-String Splitter
-Split a string based upon a separator (similar to String.prototype.split).
-*/
+/** String Splitter
+ *  Split a string based upon a separator (similar to String.prototype.split);
+ */
+
 
 const split = (string, separator = undefined, limit = 0) => {
 
@@ -104,4 +97,41 @@ const split = (string, separator = undefined, limit = 0) => {
 	return newArray;
 };
 
-console.log(split('The milk has gone bad', 's'));
+// console.log(split('The milk has gone bad', 's'));
+
+/** Binary Representation 
+ *  Write a recursive function that prints out the binary representation of a given number.
+ *
+ *  For example, the program should take 3 as an input and print 11 as output, or 25 as an 
+ *  input and print 11001 as an output. 
+ *  (Note that the binary representation of 0 should be 0).
+ */
+
+const binaryRepresentation = num => {
+
+	if(num === 0) return '';
+
+	let binaryNum = '' + Math.floor(num % 2);
+	return binaryRepresentation(Math.floor(num/2)) + binaryNum;
+};
+
+// console.log(binaryRepresentation(25));
+
+
+/**
+ * Factorial
+ * Write a recursive function that finds the factorial of a given number.
+ * The factorial of a number can be found by multiplying that number by each number
+ * between itself and one.
+ *
+ * The factorial of 5 is equal to 5 * 4 * 3 * 2 * 1 = 120
+ *
+ */
+
+
+const factorial = num => {
+	if(num === 1) return 1;
+	return num * factorial(num -1);
+};
+
+// console.log(factorial(5));
